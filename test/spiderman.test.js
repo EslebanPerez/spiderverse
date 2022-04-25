f4c831a3cbb7a9d78f4d98c7d91f848b5b57acad
EslebanPerez <eslebanperez@gmail.com>
2022-04-25 15:42:05 -0500

    Agregando prueba de unidad vacía

---
+++ test/spiderman.test.js
@@ -0,0 +1,12 @@
+describe("Test Suite Dummy Description", () => {
+    test('Case 1 Dummy', () => {
+      const resultOfSomething = 1 + 2
+      expect(resultOfSomething).toBe(18);
+    });
+  })
+
+
+  /**
+   * Este archivo se ejecuta con el comando:
+   *    npm test test/spiderman.test.js
+   */
